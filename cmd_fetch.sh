c89010cb9373e77bd144ad0224824ebeca10a2e7
X <astinx@hotmail.com>
2020-12-14 16:03:22 +0800

    add replace for site

---
+++ cmd_fetch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+BasePath=$(dirname $(readlink -f $0))
+cd $BasePath
+
+git fetch --all && git reset --hard origin/dev
+
+chmod 777 -R $BasePath && chown -R www:www $BasePath
+
+CurrentDir=`pwd`
+CurrentDir=${CurrentDir##*/}
+DB_NAME=${CurrentDir//./_}
+
+DB_PSW=`cat $BasePath/../$DB_NAME`
+
+sed -i "s/root/$DB_NAME/g" $BasePath/application/database.php
+
+sed -i "s/4rfv(IJN/$DB_PSW/g" $BasePath/application/database.php
