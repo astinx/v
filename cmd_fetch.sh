b157c450049a1b6b9ebb6815710d7dc25174ead4
X <astinx@hotmail.com>
2020-12-14 17:24:53 +0800

    自动挡添加伪静态

--- cmd_fetch.sh
+++ cmd_fetch.sh
@@ -4,7 +4,9 @@ cd $BasePath
 
 git fetch --all && git reset --hard origin/dev
 
+chattr -i $BasePath/.user.ini
 chmod 777 -R $BasePath && chown -R www:www $BasePath
+
 
 CurrentDir=`pwd`
 CurrentDir=${CurrentDir##*/}
@@ -17,3 +19,16 @@ sed -i "s/maccms10/$DB_NAME/g" $BasePath/application/database.php
 sed -i "s/root/$DB_NAME/g" $BasePath/application/database.php
 
 sed -i "s/4rfv(IJN/$DB_PSW/g" $BasePath/application/database.php
+
+echo 'location / {
+    if (!-e $request_filename) {
+        rewrite ^/index.php(.*)$ /index.php?s=$1 last;
+        rewrite ^/admin.php(.*)$ /admin.php?s=$1 last;
+        rewrite ^/api.php(.*)$ /api.php?s=$1 last;
+        rewrite ^(.*)$ /index.php?s=$1 last;
+        break;
+    }
+}' > /www/server/panel/vhost/rewrite/$CurrentDir.conf
+
+
+`/etc/init.d/nginx reload`
